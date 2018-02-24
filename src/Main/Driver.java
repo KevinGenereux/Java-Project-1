96815e9b20e3b9de19d57ad9eccd64434eeda940
TUnveiled <32398411+TUnveiled@users.noreply.github.com>
2018-02-24 14:32:57 -0500

    Finished thedriver and the shape interface

--- src/Main/Driver.java
+++ src/Main/Driver.java
@@ -1,66 +1,85 @@
 package Main;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Driver {
 	
 	public static void main(String[] args) {
+		// set up user interface
 		int choice = 1;
 		Scanner s = new Scanner(System.in);
 		
+		// main event loop
 		while (choice != 3) {
+			
+			// choose what shapes will be used
 			System.out.println("What would you like to do?\n" 
 					+ "1 - Use the default shapes\n"
 					+ "2 - Enter data for shapes\n" 
 					+ "3 - Exit the Program\n");
-			choice = Integer.parseInt(s.nextLine());
+			choice = safeScanInt(s);
+	
 
+			// switch to interpret user input
 			switch (choice) {
-			case 1:
+			case 1: // use default shapes
+				// create array for shapes, and fill with simple shapes
 				Shape[] shapes = new Shape[5];
 				shapes[0] = new Circle(5);
 				shapes[1] = new Square(5);
 				shapes[2] = new Triangle(3, 4, 5);
 				shapes[3] = new Sphere(5);
 				shapes[4] = new Cube(5);
+				
+				// method to print various information about the shapes
 				testShapes(shapes, s);
 				break;
-			case 2:
+				
+			case 2: // enter data for the shapes
+				// do this in a separate method
 				enterManualData(s);
 			}
 		}
 		
 	}
 	
+	// method to prompt, accept, and process user input about making one custom shape of each type
 	public static void enterManualData(Scanner s) {
 		double[] sides = new double[2];
 		Shape[] shapes = new Shape[5];
 		
+		// circle
 		System.out.println("What should the radius of the circle be?");
-		shapes[0] = new Circle(Double.parseDouble(s.nextLine()));
+		shapes[0] = new Circle(safeScanDouble(s));
 		
+		// square
 		System.out.println("What should the side length of the square be?");
-		shapes[1] = new Square(Double.parseDouble(s.nextLine()));
+		shapes[1] = new Square(safeScanDouble(s));
 		
+		// triangle
 		System.out.println("What should the first side length of the triangle be?");
-		sides[0] = Double.parseDouble(s.nextLine());
+		sides[0] = safeScanDouble(s);
 		System.out.println("What should the second side length of the triangle be?");
-		sides[1] = Double.parseDouble(s.nextLine());
+		sides[1] = safeScanDouble(s);
 		System.out.println("What should the third side length of the triangle be?");
-		shapes[2] = new Triangle(sides[0], sides[1], Double.parseDouble(s.nextLine()));
+		shapes[2] = new Triangle(sides[0], sides[1], safeScanDouble(s));
 		
+		// sphere
 		System.out.println("What should the radius of the sphere be?");
-		shapes[3] = new Sphere(Double.parseDouble(s.nextLine()));
+		shapes[3] = new Sphere(safeScanDouble(s));
+		
+		// cube
 		System.out.println("What should the side lenth of the cube be?");
-		shapes[4] = new Cube(Double.parseDouble(s.nextLine()));
+		shapes[4] = new Cube(safeScanDouble(s));
 		
 		testShapes(shapes,s);
 	}
 	
-	
+	// method to print information about an array of shapes
 	public static void testShapes(Shape[] shapes, Scanner s) {
 		
 		for (int i = 0; i < 5; i++) {
-			System.out.println("Shape " + i + ": " + shapes[i].toString());
+			System.out.println("\nShape " + i + ": " + shapes[i].toString());
 			if (shapes[i] instanceof TwoDimensionalShape) {
 				System.out.println("Area: " + shapes[i].getArea());
 				System.out.println("Perimeter: " + ((TwoDimensionalShape)shapes[i]).getPerimeter());
@@ -74,8 +93,47 @@ public class Driver {
 		
 		System.out.println("Press Enter to continue");
 		s.nextLine();
-		
+
 	}
 	
+	// error guarded nextDouble method
+	public static double safeScanDouble(Scanner s) {
+		// declare and initialize variables
+		double toReturn = 0;
+		boolean err = false;
+		
+		// do while to loop until valid input is entered
+		do {
+			try {
+				toReturn = s.nextDouble();
+				err = false;
+			} catch (InputMismatchException e) {
+				err = true;
+				System.out.println("Invalid input, try again!\n");
+			}
+			s.nextLine();
+		} while (err);
+		return toReturn;
+	}
+	
+	// error guarded nextInt method
+	public static int safeScanInt(Scanner s) {
+		// declare and initialize variables
+		int toReturn = 0;
+		boolean err = false;
+		
+		// do while to loop until valid input is entered
+		do {
+			try {
+				toReturn = s.nextInt();
+				err = false;
+			} catch (InputMismatchException e) {
+				err = true;
+				System.out.println("Invalid input, try again!\n");
+			}
+			s.nextLine();
+		} while (err);
+		return toReturn;
+	}
 	
 }
--- src/Main/Shape.java
+++ src/Main/Shape.java
@@ -1,7 +1,7 @@
 package Main;
 
+// abstract interface for shapes
 public abstract interface Shape {
-	
+	// both 2D and 3D shapes have area, so it should be in the shape interface
     public abstract double getArea();
-    
 }
